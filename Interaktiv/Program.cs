d1cafb0174c0f2d1de359259e0c0d7b87052243e
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-09-04 07:51:06 +0200

    Lade till if-satser och grejer

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,23 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Är du lång?");
+string tall = Console.ReadLine();
+
+// if (tall == "ja" || tall == "JA" || tall == "Ja" || tall == "jA")
+
+if (tall.ToLower() == "ja")
+{
+  Console.WriteLine("Kul för dig!");
+}
+else if (tall == "kanske")
+{
+  Console.WriteLine("okej?");
+}
+else
+{
+  Console.WriteLine("trist...");
+}
+
+
+
+
+
+Console.ReadLine();
